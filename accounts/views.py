ced8592da74b6484edcc21db6fa44eba2e38eeba
Lobogrisuwu <gabrielviveros1909@gmail.com>
2025-09-10 11:34:39 -0600

    views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile  # 👈 correcto: desde accounts.models
+from .models import Profile
 
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # Asegura que exista el Profile
             Profile.objects.get_or_create(user=user)
             login(request, user)
             return redirect("feed")
