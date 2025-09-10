7ab665b74491f7f154742be5b6486331309d278f
Lobogrisuwu <gabrielviveros1909@gmail.com>
2025-09-10 11:34:12 -0600

    urls.py

--- instax/urls.py
+++ instax/urls.py
@@ -1,8 +1,9 @@
+# instax/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),  # <-- esto es clave
-    path("", include("posts.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')),         # feed
+    path('accounts/', include('accounts.urls')),  # login/registro
 ]
